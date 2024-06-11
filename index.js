6c273a5a3536e7ded0d05fa6f210b9b99fdd788a
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-06-11 17:13:31 -0500

    generated a markdown

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 
-const { default: Choices } = require("inquirer/lib/objects/choices");
+const fs = require("fs");
+const generateMarkdown = require("./Develop/utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -61,7 +62,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName,generateMarkdown(data))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
